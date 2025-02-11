000052e08006dacec93c02ca4e96bfca9e6ac920
Vadim Khalin <khalinvadim3@gmail.com>
2025-02-12 00:40:10 +0300

    Make the class Snake

--- the_snake.py
+++ the_snake.py
@@ -14,20 +14,27 @@ DOWN = (0, 1)
 LEFT = (-1, 0)
 RIGHT = (1, 0)
 
+KEYS = {
+    pg.K_UP: UP,
+    pg.K_DOWN: DOWN,
+    pg.K_LEFT: LEFT,
+    pg.K_RIGHT: RIGHT,
+}
+
 # Константы цвета:
-BOARD_BACKGROUND_COLOR = (200, 200, 200)
-BORDER_COLOR = (93, 216, 228)
+BOARD_BACKGROUND_COLOR = (128, 128, 128)
+BORDER_COLOR = (128, 128, 128)
 APPLE_COLOR = (255, 0, 0)
 SNAKE_COLOR = (0, 255, 0)
 
 # Скорость движения змейки:
-SPEED = 20
+SPEED = 10
 
 # Настройка игрового окна:
 screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
 
 # Заголовок окна игрового поля:
-pg.display.set_caption('Змейка')
+pg.display.set_caption('Змейка \\ ESC - выход')
 
 # Настройка времени:
 clock = pg.time.Clock()
@@ -67,7 +74,7 @@ class GameObject:
 
 
 class Apple(GameObject):
-    """Make new class Apple inhireteed by GameObject."""
+    """Make new class Apple inhireted by GameObject."""
 
     def __init__(
         self,
@@ -87,7 +94,6 @@ class Apple(GameObject):
                             randint(0, GRID_HEIGHT - 1) * GRID_SIZE)
             if new_position not in occupied_cells:
                 self.position = new_position
-                print(self.position)
                 return self.position
 
     def draw(
@@ -97,69 +103,113 @@ class Apple(GameObject):
         self.draw_one_cell(self.position)
 
 
-
 class Snake(GameObject):
-    pass
+    """Make new class Snake inhireted by GameObject."""
+
+    def __init__(
+        self,
+        body_color=SNAKE_COLOR
+    ):
+        super().__init__(body_color)
+        self.reset()
+
+    def get_head_positions(
+        self
+    ):
+        """Define the head of the snake."""
+        return self.positions[0]
+
+    def reset(
+        self
+    ):
+        """Reset the snake."""
+        self.length = 1
+        self.direction = choice([UP, DOWN, RIGHT, LEFT])
+        self.positions = [(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)]
+        self.last = None
+
+    def update_directions(
+        self,
+        new_position
+    ):
+        """Update the directions."""
+        if new_position:
+            self.direction = new_position
+
+    def move(
+        self
+    ):
+        """The movement of the snake."""
+        x, y = self.get_head_positions()
+        x_1, y_1 = self.direction
+
+        head = ((x + x_1 * GRID_SIZE) % SCREEN_WIDTH,
+                (y + y_1 * GRID_SIZE) % SCREEN_HEIGHT)
+
+        self.positions.insert(0, head)
+        if len(self.positions) > self.length:
+            self.last = self.positions.pop()
+        else:
+            self.last = None
+
+    def draw(
+        self
+    ):
+        """Draw the snake."""
+        self.draw_one_cell(self.get_head_positions())
+        if self.last:
+            self.draw_one_cell(self.last, BOARD_BACKGROUND_COLOR)
+
+
+def handle_keys(snake):
+    """Check the keybord."""
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            raise SystemExit
+        elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
+            pg.quit()
+            raise SystemExit
+
+        if event.type == pg.KEYDOWN:
+            new_direction = KEYS.get(event.key)
+            if (
+                new_direction
+                and new_direction[0] != -snake.direction[0]
+                and new_direction[1] != -snake.direction[1]
+            ):
+                snake.update_directions(new_direction)
 
 
 def main():
+    """Start the game."""
     # Инициализация pg:
     pg.init()
     # Тут нужно создать экземпляры классов.
-    apple = Apple([(180, 200)])
+    snake = Snake()
+    apple = Apple(snake.positions)
+
+    screen.fill(BOARD_BACKGROUND_COLOR)
 
     while True:
         clock.tick(SPEED)
-        apple.draw()
+        handle_keys(snake)
+        snake.move()
 
+        if snake.positions[0] == apple.position:
+            snake.length += 1
+            apple.position = apple.randomize_position(snake.positions)
+
+        if snake.get_head_positions() in snake.positions[4:]:
+            snake.reset()
+            screen.fill(BOARD_BACKGROUND_COLOR)
+
+        apple.draw()
+        snake.draw()
+
+        pg.display.set_caption(f'ESC - выход | \t\t\t Змейка \t\t\t | Длина: {snake.length}')
         pg.display.update()
 
 
 if __name__ == '__main__':
     main()
-
-
-# Метод draw класса Apple
-# def draw(self):
-#     rect = pg.Rect(self.position, (GRID_SIZE, GRID_SIZE))
-#     pg.draw.rect(screen, self.body_color, rect)
-#     pg.draw.rect(screen, BORDER_COLOR, rect, 1)
-
-# # Метод draw класса Snake
-# def draw(self):
-#     for position in self.positions[:-1]:
-#         rect = (pg.Rect(position, (GRID_SIZE, GRID_SIZE)))
-#         pg.draw.rect(screen, self.body_color, rect)
-#         pg.draw.rect(screen, BORDER_COLOR, rect, 1)
-
-#     # Отрисовка головы змейки
-#     head_rect = pg.Rect(self.positions[0], (GRID_SIZE, GRID_SIZE))
-#     pg.draw.rect(screen, self.body_color, head_rect)
-#     pg.draw.rect(screen, BORDER_COLOR, head_rect, 1)
-
-#     # Затирание последнего сегмента
-#     if self.last:
-#         last_rect = pg.Rect(self.last, (GRID_SIZE, GRID_SIZE))
-#         pg.draw.rect(screen, BOARD_BACKGROUND_COLOR, last_rect)
-
-# Функция обработки действий пользователя
-# def handle_keys(game_object):
-#     for event in pg.event.get():
-#         if event.type == pg.QUIT:
-#             pg.quit()
-#             raise SystemExit
-#         elif event.type == pg.KEYDOWN:
-#             if event.key == pg.K_UP and game_object.direction != DOWN:
-#                 game_object.next_direction = UP
-#             elif event.key == pg.K_DOWN and game_object.direction != UP:
-#                 game_object.next_direction = DOWN
-#             elif event.key == pg.K_LEFT and game_object.direction != RIGHT:
-#                 game_object.next_direction = LEFT
-#             elif event.key == pg.K_RIGHT and game_object.direction != LEFT:
-#                 game_object.next_direction = RIGHT
-
-# Метод обновления направления после нажатия на кнопку
-# def update_direction(self):
-#     if self.next_direction:
-#         self.direction = self.next_direction
-#         self.next_direction = None
